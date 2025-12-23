24f8195dfe295fbdae1eebb304042afa2f42ddbc
Gowtham-beep <gn86923@gmail.com>
2025-12-23 16:38:23 +0530

    chore: remove unused variable declaration
    - Eliminated the unused 'unused2' variable from index.ts to clean up dead code and improve code quality. The change maintains the existing structure while reducing unnecessary declarations.

--- packages/eslint-config/base.cjs
+++ packages/eslint-config/base.cjs
@@ -4,19 +4,28 @@ module.exports = {
     es2021: true,
   },
 
+  parser: '@typescript-eslint/parser',
+
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
 
-  plugins: ['turbo', 'only-warn'],
+  plugins: ['@typescript-eslint', 'turbo'],
 
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
 
   rules: {
+    // Disable base rule (conflicts with TS)
+    'no-unused-vars': 'off',
+
+    // Enable TS-aware rule
+    '@typescript-eslint/no-unused-vars': ['error'],
+
     'turbo/no-undeclared-env-vars': 'warn',
   },
 
